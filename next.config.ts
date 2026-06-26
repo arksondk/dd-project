8da3e9c9ae19528e9884225d0d2ce0d2c6a7c403
Rum Chen <i.mr.redrum@gmail.com>
2026-06-26 22:16:42 +0800

    feat: 新增 GitHub Pages 部署工作流程及配置 Next.js 輸出設置

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,19 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
+
+const isProduction = process.env.NODE_ENV === 'production'
+const REPO_PREFIX = '/multi-twitch-stream'
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-};
 
-export default nextConfig;
+  // Setting up GitHub Pages
+  output: 'export',
+  basePath: isProduction ? REPO_PREFIX : '',
+  assetPrefix: isProduction ? `${REPO_PREFIX}/` : '',
+  images: {
+    unoptimized: true,
+  },
+}
+
+export default nextConfig
